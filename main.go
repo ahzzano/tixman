d6b0543685cb0c60a8b6108b268ec2bacba28a88
William Santos <wsantos@Williams-MacBook-Air.local>
2025-01-31 16:15:21 +0800

    Some major changes

--- main.go
+++ main.go
@@ -1,22 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
 	"os"
+	"os/signal"
 
 	"github.com/bwmarrin/discordgo"
 )
 
+var (
+	Token = flag.String("token", "", "Auth Token")
+	App   = flag.String("app", "", "Application ID")
+	Guild = flag.String("guild", "", "GuildID")
+)
+
+var commands = []*discordgo.ApplicationCommand{
+	{
+		Name:        "echo",
+		Description: "Hello World",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Name:        "message",
+				Description: "what",
+				Required:    true,
+			},
+		},
+	},
+}
+
+func setupHandlers(sess *discordgo.Session) {
+	sess.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as %s", r.User.String())
+	})
+}
+
 func main() {
-	token := os.Getenv("TIX_TOKEN")
-	println(token)
+	flag.Parse()
 
-	bot, err := discordgo.New("Bot " + token)
-
-	if err != nil {
-		panic(fmt.Errorf("Error: Unable to create bot %s", err))
+	if *Token == "" {
+		log.Fatalf("No token was supplied. Supply the token via the token flag")
+		return
 	}
 
-	fmt.Println("Hello World!")
-	fmt.Println("%s", bot)
+	session, err := discordgo.New("Bot " + *Token)
+
+	if err != nil {
+		log.Fatalf("Unable to open session: %s", err)
+	}
+
+	setupHandlers(session)
+
+	err = session.Open()
+
+	if err != nil {
+		log.Fatalf("Could not open session: %s", err)
+	}
+
+	log.Printf("Session is now currently running")
+
+	sigch := make(chan os.Signal, 1)
+
+	signal.Notify(sigch, os.Interrupt)
+	<-sigch
+
+	err = session.Close()
+
+	if err != nil {
+		log.Printf("Could not close gracefully: %s", err)
+	}
+
+	log.Printf("Session has now closed. Saynora...")
+
 }
